e3fe8b99091a0e68ac495674e537e1f53d429619
ibrahimsak <ibrahim.sak9@gmail.com>
2020-11-24 22:22:37 +0300

    lesson 3:Model Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TODOAPP';
+  items=[
+    {description:'Kahvaltı',action:'no'},
+    {description:'Sinema',action:'no'},
+    {description:'Spor',action:'no'},
+    {description:'Ders Çalış',action:'no'}
+  ];
 }
